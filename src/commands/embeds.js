8b3e7ac199d36a6f7074c91208cfb355122ad768
KrazyKleb <124646709+KrazyKleb@users.noreply.github.com>
2023-08-31 01:43:04 +1000

    Changed Rules Embed command to /rulesembed

--- src/commands/embeds.js
+++ src/commands/embeds.js
@@ -12,7 +12,7 @@ module.exports = {
         .setTitle(`Embed list`)
         .setFields(
             { name: `Information Embed (/infoembed)`, value: `Information Embed, goes in the <#1146060384638349352> channel` },
-            { name: `Rules Embed (/infoembed)`, value: `Rules Embed, goes in the <#1145921287319990413> channel` },
+            { name: `Rules Embed (/rulesembed)`, value: `Rules Embed, goes in the <#1145921287319990413> channel` },
         )
 
         interaction.reply({ embeds: [embed], ephemeral: true })
